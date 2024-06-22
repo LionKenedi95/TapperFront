65319f9dae0ec5a4e75d5e40b3e0bbcdd2283dd7
Vadim Demchenko <v.demchenko@revizto.com>
2024-06-22 20:42:22 +0400

    improve styles

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -20,10 +20,10 @@ const counter = ref('150067')
     </div>
 
     <div class="home-actions">
-      <div class="action">action 1</div>
-      <div class="action">action 2</div>
-      <div class="action">action 3</div>
-      <div class="action">action 4</div>
+      <div class="action">+ 100 тысяч ⚛️</div>
+      <div class="action">Тапай с друзьями и ускоряйте прогресс</div>
+      <div class="action">Ежедневный подарок</div>
+      <div class="action">Прокачать ⚡</div>
     </div>
 
     <div class="home-promo"></div>
@@ -32,7 +32,7 @@ const counter = ref('150067')
       <h1 class="text-center">
         <n-gradient-text :gradient="{
           deg: 75,
-          from: '#ff7e5f',
+          from: '#7E3F8F',
           to: '#91eae4'
         }">
           {{ counter }}
@@ -54,46 +54,71 @@ const counter = ref('150067')
 <style scoped>
 .home-grid {
   display: grid;
-  padding: 32px 8px 8px;
-  height: 100%;
+  height: calc(100% - 64px);
   grid-template-columns: auto;
-  grid-template-rows: 75px 50px auto 100px 250px 35px;
+  grid-template-rows: 125px auto auto 100px 250px 35px;
+  
 }
 
-.home-title {}
+.home-title {
+  padding: 52px 24px 0;
+  background-color: #12100E;
+  box-shadow: 0 4px 8px rgba(99, 226, 183, .8), 0 6px 20px rgba(99, 226, 183, .8);
+  border-radius: 0 0 10px 10px;
+}
 .home-actions {
   display: grid;
   grid-template-columns: 1fr 1fr;
+  grid-template-rows: min-content min-content;
   gap: 8px;
+  padding: 24px 24px 8px;
 }
-.home-promo {}
-.home-counter {}
+.home-promo {
+  padding: 0 24px;
+}
+.home-counter {
+  display: flex;
+  align-items: end;
+  justify-content: center;
+  padding: 0 24px;
+
+  h1 .n-gradient-text {
+    font-family: sans-serif;
+    font-weight: 800;
+  }
+}
 .home-tapper {
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 0 24px;
 }
 .home-energy {
   display: grid;
   grid-template-columns: 36px calc(100% - 86px);
   align-items: center;
   gap: 16px;
+  padding: 0 24px;
 }
 
 .home-title__info {
   display: flex;
-  align-items: center;
+  align-items: left;
   gap: 16px;
-  font-size: 12px;
+  font-size: 16px;
 }
 
 .action {
-  border: 1px solid #243834;
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 8px;
   border-radius: 8px;
+  font-size: 12px;
+  height: 55px;
+  text-align: center;
+  border: 1px solid #2274A5;
+  color: #ffffff;
 }
 
 .tapper {
@@ -101,7 +126,6 @@ const counter = ref('150067')
   height: 150px;
   border-radius: 50%;
   background-color: #86a8e7;
-  margin-bottom: 50px;
 }
 
 .energy-bar {
@@ -110,14 +134,14 @@ const counter = ref('150067')
   border-radius: 4px;
   width: 100%;
   border: 1px solid;
-  border-color: linear-gradient(45deg, #ff7e5f, #feb47b, #86a8e7, #91eae4);
+  border-color: linear-gradient(45deg, #ff7e5f, #feb47b, #F6F930, #3A7D44, #3A7D44);
 
   > div {
     display: block;
     height: 14px;
     width: 100%;
     border-radius: 4px;
-    background: linear-gradient(45deg, #ff7e5f, #feb47b, #86a8e7, #91eae4);
+    background: linear-gradient(25deg, #ff7e5f, #feb47b, #F6F930, #3A7D44, #3A7D44);
   }
 }
 </style>
