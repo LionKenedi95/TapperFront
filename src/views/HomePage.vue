7a1ec1a4c39e5cc69d9931ad197db56f2905f309
Vadim Demchenko <v.demchenko@revizto.com>
2024-07-05 20:02:41 +0400

    New design

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,96 +11,64 @@ const onClickAction = () => {
   router.push({ name: Routes.ActionPage })
 }
 
-const tapperWrapperRef = ref<HTMLElement | null>(null)
+// const tapperWrapperRef = ref<HTMLElement | null>(null)
 
-const signVariants = ['⚛️', '💚', '🩵', '✨', '🎈', '🎁', '🎟️', '🎫', '⚛️', '⚛️']
-const getSignContent = () => {
-  const rareFactor = Math.floor(Math.random() * 10);
+// const signVariants = ['⚛️', '💚', '🩵', '✨', '🎈', '🎁', '🎟️', '🎫', '⚛️', '⚛️']
+// const getSignContent = () => {
+//   const rareFactor = Math.floor(Math.random() * 10);
 
-  if (rareFactor !== 9) {
-    return signVariants[0]
-  }
+//   if (rareFactor !== 9) {
+//     return signVariants[0]
+//   }
 
-  const i = Math.floor(Math.random() * 10);
-  return signVariants[i]
-}
+//   const i = Math.floor(Math.random() * 10);
+//   return signVariants[i]
+// }
 
-const drawSign = (touch: Touch) => {
-  const wrappeBox = tapperWrapperRef.value.getBoundingClientRect()
-  const clientY = touch.pageY - wrappeBox.y
+// const drawSign = (touch: Touch) => {
+//   const wrappeBox = tapperWrapperRef.value.getBoundingClientRect()
+//   const clientY = touch.pageY - wrappeBox.y
 
-  const sign = document.createElement('div')
-  sign.className = 'sign'
-  sign.style.left = touch.pageX + 'px'
-  sign.style.top = clientY + 'px'
-  sign.textContent = getSignContent()
+//   const sign = document.createElement('div')
+//   sign.className = 'sign'
+//   sign.style.left = touch.pageX + 'px'
+//   sign.style.top = clientY + 'px'
+//   sign.textContent = getSignContent()
 
-  tapperWrapperRef.value.appendChild(sign)
+//   tapperWrapperRef.value.appendChild(sign)
 
-  setTimeout(() => {
-    sign.remove()
-  }, 500)
-}
+//   setTimeout(() => {
+//     sign.remove()
+//   }, 500)
+// }
 
-const onClickTapper = (e: TouchEvent) => {
-  counter.increment(e.touches.length)
-  if (!tapperWrapperRef.value) return
+// const onClickTapper = (e: TouchEvent) => {
+//   counter.increment(e.touches.length)
+//   if (!tapperWrapperRef.value) return
 
-  Array.from(e.touches).forEach((touch: Touch) => {
-    drawSign(touch)
-  })
+//   Array.from(e.touches).forEach((touch: Touch) => {
+//     drawSign(touch)
+//   })
 
-  e.preventDefault()
-  e.stopPropagation()
-  e.stopImmediatePropagation()
-}
+//   e.preventDefault()
+//   e.stopPropagation()
+//   e.stopImmediatePropagation()
+// }
 
-const onMoveTapper = (e: TouchEvent) => {
-  e.preventDefault()
-  e.stopPropagation()
-  e.stopImmediatePropagation()
-}
+// const onMoveTapper = (e: TouchEvent) => {
+//   e.preventDefault()
+//   e.stopPropagation()
+//   e.stopImmediatePropagation()
+// }
 </script>
 
 <template>
   <main class="home-grid default-page-height">
-    <UserInfoHeader />
-
-    <div class="home-actions">
-      <div class="action" @click="onClickAction"><span class="text-18">⚛️</span> +100 тысяч</div>
-      <div class="action" @click="onClickAction">Тапай с друзьями и ускоряйте прогресс</div>
-      <div class="action" @click="onClickAction"><span class="text-18">🎁</span> Ежедневный подарок</div>
-      <div class="action" @click="onClickAction"><span class="text-18">⚡</span> Прокачать</div>
-    </div>
-
-    <div class="home-promo"></div>
-
-    <div class="home-counter">
-      <h1 class="text-center">
-        <n-gradient-text :gradient="{
-          deg: 75,
-          from: '#7E3F8F',
-          to: '#91eae4'
-        }">
-          {{ counter.count }}
-        </n-gradient-text>
-        ⚛️
-      </h1>
-    </div>
-
-    <div
-      ref="tapperWrapperRef"
-      class="home-tapper"
-      @touchstart="onClickTapper"
-      @touchmove.stop.prevent="onMoveTapper"
-      @touchend.stop.prevent
-    >
-      <div class="tapper"></div>
-    </div>
-
-    <div class="home-energy">
-      <h1>⚡</h1><div class="energy-bar"><div /></div>
-    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
   </main>
 </template>
 
@@ -108,90 +76,13 @@ const onMoveTapper = (e: TouchEvent) => {
 .home-grid {
   display: grid;
   grid-template-columns: auto;
-  grid-template-rows: 82px 125px 75px 50px 200px 35px;
-}
-
-.home-title {
-}
-.home-actions {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: min-content min-content;
-  gap: 8px;
-  padding: 24px 24px 8px;
-}
-.home-promo {
-  padding: 0 24px;
-}
-.home-counter {
-  display: flex;
-  align-items: end;
-  justify-content: center;
-  padding: 0 24px;
-
-  h1 {
-    left: 24px;
-    position: relative;
-  }
-
-  h1 .n-gradient-text {
-    font-family: sans-serif;
-    font-weight: 800;
-  }
-}
-.home-tapper {
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0 24px;
-}
-.home-energy {
-  display: grid;
-  grid-template-columns: 36px calc(100% - 86px);
-  align-items: center;
-  gap: 16px;
-  padding: 0 24px;
-}
-
-.action {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 8px;
-  border-radius: 8px;
-  font-size: 10px;
-  height: 50px;
-  text-align: center;
-  border: 1px solid #2274A5;
-  color: #ffffff;
-  background-color: #12100E;
-}
-
-.tapper {
-  width: 175px;
-  height: 175px;
-  border-radius: 50%;
-  background-color: #86a8e7;
-  box-shadow: 0 0 8px #86a8e7;
-}
-
-.energy-bar {
-  display: block;
-  height: 16px;
-  border-radius: 4px;
-  width: 100%;
-  border: 1px solid;
-  border-color: linear-gradient(45deg, #ff7e5f, #feb47b, #F6F930, #3A7D44, #3A7D44);
+  grid-template-rows: 80px 86px 321px 45px 91px;
 
   > div {
-    display: block;
-    height: 14px;
-    width: 100%;
-    border-radius: 4px;
-    background: linear-gradient(25deg, #ff7e5f, #feb47b, #F6F930, #3A7D44, #3A7D44);
+    border: 1px solid white;
   }
 }
+
 </style>
 
 <style>
--- src/views/WelcomePage1.vue
+++ src/views/WelcomePage1.vue
@@ -1,53 +1,18 @@
 <script setup lang="ts">
 import router from '@/router';
 import { Routes } from '@/constants/Routes'
-import { CheckmarkStarburst16Filled } from '@vicons/fluent' 
 
 const onClickStart = () => {
   router.push({ name: Routes.Home })
 }
+
+onClickStart()
 </script>
 
 <template>
-  <main class="flex-vertical-center-center">
-    <div class="flex-center-center">
-      <h1 class="text-center">
-        <n-gradient-text >Привет, это Tapper </n-gradient-text>
-      </h1>
-      <n-icon size="40" color="#18623b">
-        <CheckmarkStarburst16Filled />
-      </n-icon>
-    </div>
-    
-    <h2 class="text-center">1. Тапай и получай <n-gradient-text>Атомы</n-gradient-text> ⚛️</h2>
-    <h2 class="text-center">
-      2. Обменивай ⚛️ на<br> 🎁 призы от маркетплейсов,<br> 👏 эвенты с инфлюэнсерами<br> и 💵 реальные деньги!
-    </h2>
-    <n-button strong round class="start-button" type="success" size="large" @click="onClickStart">
-      Старт <span class="icon-start">👇🏻</span>
-    </n-button>
+  <main class="">
   </main>
 </template>
 
 <style scoped>
-.start-button {
-  padding: 36px;
-  font-size: 24px;
-  margin-top: 48px;
-}
-
-.icon-start {
-  position: relative;
-  animation-name: tap;
-  animation-duration: 1s;
-  animation-iteration-count: infinite;
-  animation-timing-function: ease-in;
-}
-
-@keyframes tap {
-  0%   { top: 0px; }
-  50%  { top: -2px; }
-  75%  { top: 0px; }
-  100% { top: 2px; }
-}
 </style>
